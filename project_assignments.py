8ab54f0c5d5f33da0bfdd9514b556c24eb240b21
Shiva Amiri <152985980+shivaaa15@users.noreply.github.com>
2024-10-14 14:06:14 +0330

    Add files via upload

---
+++ project_assignments.py
@@ -0,0 +1,33 @@
+import random
+import clickhouse_connect
+
+# Connect to ClickHouse
+client = clickhouse_connect.get_client(host='192.168.100.217', port=8123)
+
+
+table_name = 'project_assignments'
+
+# Create table project_assignments
+client.command(f'''
+CREATE TABLE IF NOT EXISTS {table_name} (
+    ID Int32,
+    Project_Name String
+) ENGINE = MergeTree()
+ORDER BY (ID)
+''')
+
+# Define project names
+project_names = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta"]
+
+# list of data with common project name for 50 ID
+data = []
+
+for id in range(1, 51):  
+    project = random.choice(project_names)  # انتخاب نام پروژه به‌صورت تصادفی از لیست
+    data.append((id, project))
+
+
+client.insert(table_name, data, column_names=['ID', 'Project_Name'])
+
+print("Data inserted successfully into ClickHouse!")
+
---
+++ salary.py
@@ -0,0 +1,44 @@
+import random
+import clickhouse_connect
+import names
+
+# Connect to ClickHouse
+client = clickhouse_connect.get_client(host='192.168.100.217', port=8123)
+
+# Create table
+table_name = 'company1403'
+
+client.command(f'''
+CREATE TABLE IF NOT EXISTS {table_name} (
+    ID Int32,
+    NAME String,
+    DEPARTMENT String,
+    SALARY Int32
+) ENGINE = MergeTree()
+ORDER BY (ID)
+''')
+
+# Define the parameters
+ids = range(1, 51)
+DEPARTMENT_NAME = ['Support', 'Data', 'DevOps', 'Development', 'Accounting', 'SRE', 'HR']
+
+# Function to generate data for each ID
+def generate_data():
+    rows = []
+
+    for ID in ids:
+        NAME = names.get_full_name()
+        DEPARTMENT = random.choice(DEPARTMENT_NAME)
+        SALARY = random.randint(18000000, 35000000)
+        rows.append((ID, NAME, DEPARTMENT, SALARY))
+    
+    return rows
+
+# Generate data and insert it into ClickHouse
+data = generate_data()
+
+# Insert data into the table
+client.insert(table_name, data, column_names=['ID', 'NAME', 'DEPARTMENT', 'SALARY'])
+
+print("Data inserted successfully into ClickHouse!")
+
